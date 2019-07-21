8017a24fe2ab97038f2823ba0646bc8bf3eade92
Eric Jobin <ejobin@gmail.com>
2019-07-21 14:54:32 -0400

    Added rows to table based on input values

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,3 +1,50 @@
 // Train Schedule
 
-//#addTrainButton
+//Global Variables
+
+var trainName;
+var destination;
+var firstTrainTime;
+var frequency;
+
+
+$(document).ready(function() {
+
+    $('#addTrainButton').on("click", function(e) {
+        e.preventDefault();
+        var input = $('input');
+        console.log('submit clicked');
+        console.log(input);
+
+        trainName=inputTrainName.value;
+        destination=inputDestination.value;
+        firstTrainTime=inputFirstTrainTime.value;
+        frequency=inputFrequency.value;
+
+
+        input.val('');
+
+        // Train Name	Destination	Frequency (min)	Next Arrival (calculated)	Minutes Away (calculated)
+        
+        var newRow = `
+            <tr><td>${trainName}</td>
+            <td>${destination}</td>
+            <td>${frequency}</td>
+            <td>7:00 PM</td>
+            <td>35</td> 
+            </tr>`
+        
+          // Append the new row to the table
+          $("#trainRef").append(newRow);
+
+
+
+
+
+
+
+    });
+
+});
+
+//#addTrainButton trainRef

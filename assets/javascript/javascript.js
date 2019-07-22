f43ccd769f932968ae8f9e9a877e652c0d200f5f
Eric Jobin <ejobin@gmail.com>
2019-07-22 00:15:23 -0400

    Coded train schedule updating in real time (well every 6 seconds...)

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -12,11 +12,12 @@ var frequency;
 var nextArrival;
 var timeAway;
 var i=0;
+var dbTrainObj=[];
 
 //Calculate time to next train
 function calcTrainTimes(ftt, frq){
     var tFrequency = frq;
-    var firstTrainTimeConverted = moment(ftt, "HH:mm")
+    var firstTrainTimeConverted = moment(ftt, "HH:mm").subtract(1, "years");
     var diffTime = moment().diff(moment(firstTrainTimeConverted), "minutes");
     var tRemainder = diffTime % tFrequency;
     var tMinutesTillTrain = tFrequency - tRemainder;
@@ -67,25 +68,36 @@ $(document).ready(function() {
         destination = childSnapshot.val().destination;
         firstTrainTime = childSnapshot.val().trainTime;
         frequency = childSnapshot.val().freq;
+
+        
+        dbTrainObj[i] = {
+            index: i,
+            name: trainName,
+            destination: destination,
+            trainTime: firstTrainTime,
+            freq: frequency,
+        };
+
         if (trainName != undefined){
             appendTrain()
+            i++
         };
-        i++
+        
     }),
     function(errorObject) {
         console.log("The read failed: " + errorObject.code);
     };
 });
 
-//-------------------------------------------------Trying to update in real time -----------------------------------------
-
-// function updateTrains(){
-//     x=0
-//     a=`nxArr${x}`;
-//     b=`frq${x}`;
-//     calcTrainTimes(a, b)
-//     $(`#nextArrival${x}`).text(nextArrival);
-//     $(`#timeAway${x}`).text(timeAway);
-
-//     nextArrival
-// }
+//Function to updated trains
+function updateTrains(){
+    for (var x=0; x<dbTrainObj.length; x++){
+        var a=dbTrainObj[x].trainTime;
+        var b=dbTrainObj[x].freq;
+        calcTrainTimes(a, b)
+        $(`#nxArr${x}`).text(nextArrival);
+        $(`#timeAway${x}`).text(timeAway);
+    }
+    nextArrival
+}
+setInterval(updateTrains, 6000); //Running update every 6 seconds
